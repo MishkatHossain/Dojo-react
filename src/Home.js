bba653346951c7ff68d014bb3dbfc773bb2c5391
Mishkat Hossain <hossainmishkat0@gmail.com>
2023-09-10 20:49:14 +0600

    10step

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,23 @@
+import { useState } from 'react';
+
+
+
 const Home = () => {
-
-    const handleClick = () => {
-        console.log("hello world");
-    }
-
-
-
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'The world will burn', author: 'mario', id:1},
+        {title: 'Welcome', body: 'The world shall know pain', author: 'Nagato', id:2},
+        {title: 'YouTube', body: 'Like share subscribe', author: 'mario', id:3}
+    ]);
 
 
     return ( 
         <div className="home">
-            <h2>Homepage of this Hyper</h2>
-            <button onClick={handleClick}>Click here</button>
+            {blogs.map((blog) => (
+                <div className="Blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written By {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
