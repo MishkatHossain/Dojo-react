7fe14b68ce8d75574bd7613b8ff3c56d000b1342
Mishkat Hossain <hossainmishkat0@gmail.com>
2023-09-12 22:43:06 +0600

    custom hooks

--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,24 @@
-import { useState, useEffect } from 'react';
-import Bloglist from './blog-list'
+import Bloglist from './blog-list';
+import useFetch from './usefetch';
+
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-
-        {title: 'My new website', body: 'The world will burn', author: 'mario', id:1},
-        {title: 'Welcome', body: 'The world shall know pain', author: 'Nagato', id:2},
-        {title: 'YouTube', body: 'Like share subscribe', author: 'mario', id:3},
-        {title: 'StarLight Saber', body: 'Darkest Night is faced by me', author: 'Duplicate', id:4},
-        {title: 'Cricket', body: 'Off break is the new Leg break', author: 'Brett Lee', id:5},
-        {title: 'Coding', body: 'The more you code the less you fail', author: 'Mishkat', id:6},
-        {title: 'Life', body: 'Life lesson is hard and crucial. Let it teach you a lesson', author: 'Mishkat', id:7}
     
-    ]); 
-
-    const [name,setName] = useState('mario');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs);
-    }
-
-    useEffect(()=>{
-        console.log('use effect ran')
-        console.log(name);
-    }, [name]);
-
+    const {data, isPending, error} = useFetch('  http://localhost:8000/blogs')
+    
 
 
     return ( 
         <div className="home">
-            <Bloglist blogs={blogs} title = "All blogs" handleDelete={handleDelete}/>
-            <button onClick={()=> setName('Luigi')}>Change Name</button>            
-            <p>{name}</p>
-            
-
-            <Bloglist blogs={blogs.filter((blog)=> blog.author==='mario')} title = "Mario's blogs" handleDelete={handleDelete}/>
-            <Bloglist blogs={blogs.filter((blog)=> blog.author==='Mishkat')} title = "Mishkat's blogs" handleDelete={handleDelete}/>
-            <Bloglist blogs={blogs.filter((blog)=> blog.id%2===1)} title = "Odd break" handleDelete={handleDelete}/>
+            {error && <div> {error} </div>}
+            {isPending && <div>Loading...</div>}
+            {data && <Bloglist blogs={data} title="All blogs" />}
         </div>
      );
 }
  
 export default Home;
+
+
+// npx json-server --watch data/db.json --port 8000
--- src/blog-list.js
+++ src/blog-list.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs, title}) => {
 
 
     return ( 
@@ -9,7 +9,6 @@ const BlogList = ({blogs, title, handleDelete}) => {
                     <h2>{ blog.title }</h2>
                     <p1>{ blog.body }</p1>
                     <p>Written By {blog.author}</p>
-                    <button onClick={() => handleDelete(blog.id)}> delete blog  </button>
                 </div>
             ))}
         </div>
